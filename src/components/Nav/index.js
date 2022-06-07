d53c39b806b6aaa0f81c35a847e05cedade25e2a
sp381 <sarah.powell381@gmail.com>
2022-06-07 11:22:23 -0700

    adding files

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
+  function categorySelected() {
+      console.log("hello")
+  }
+  
 function Nav() {
 
     return (
-        <header>
-              <h2>
+<header>
+  <h2>
     <a href="/">
       <span role="img" aria-label="camera"> 📸</span> Oh Snap!
     </a>
@@ -19,9 +37,19 @@ function Nav() {
       <li>
         <span>Contact</span>
       </li>
+       {categories.map((category) => (
+        <li
+          className="mx-1"
+          key={category.name}
+        >
+          <span onClick={categorySelected} >
+            {category.name}
+          </span>
+        </li>
+      ))}
     </ul>
   </nav>
-        </header>
+</header>
     );
 }
 
