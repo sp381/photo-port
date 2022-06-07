a26b82056a05bb7b455611561c60553e5f25ce56
sp381 <sarah.powell381@gmail.com>
2022-06-07 12:33:39 -0700

    finished testing flies

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
 afterEach(cleanup);
+
 describe('About component', () => {
-    //First Test
-    it('renders', () => {
-        render(<About />);
-    });
-
-    //Second Test
-    it('matches snapshot DOM node structure', () => {
-        //render About
-    });
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
-
-const { asFragment } = render(<About />);
-expect(asFragment()).toMatchSnapshot();
-
